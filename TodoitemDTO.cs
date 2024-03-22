6102db5e7384586214c60d54bc039aed913865c2
Manuel Munoz <manuel@maddevelopment.com>
2024-03-22 15:38:56 -0600

    Add project files.

---
+++ TodoitemDTO.cs
@@ -0,0 +1,14 @@
+﻿namespace APIMinima
+{
+    public class TodoitemDTO
+    {
+
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public bool IsComplete { get; set; }
+
+        public TodoitemDTO() { }
+        public TodoitemDTO(Todo TodoItem) =>
+            (Id, Name, IsComplete) = (TodoItem.Id, TodoItem.Name, TodoItem.IsComplete);
+    }
+}
